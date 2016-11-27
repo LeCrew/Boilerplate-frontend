f7fbdd9134255a8745ac97be7c848082bc4b6447
Luc Terracher <lucterracher@gmail.com>
2016-11-27 20:49:48 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+const STYLES = require('../../public/less/main.less');
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styles: [STYLES, require('./app.component.less')]
+})
+
+export class AppComponent { }
